86d5316d3e01880698ac787488a7406fa04106ea
Ravula Nishanth <nishanth.ravula@outlook.com>
2021-12-24 17:02:16 +0530

    Minor update

--- src/components/helpers/Menu.js
+++ src/components/helpers/Menu.js
@@ -1,7 +1,7 @@
 const Menu = [
   { id: 1, name: " World of Kryss", link: "#" },
   { id: 1, name: "Models", link: "#" },
-  { id: 1, name: " Techmology", link: "#" },
+  { id: 1, name: " Technology", link: "#" },
   { id: 1, name: "Contact", link: "#" },
 ];
 
